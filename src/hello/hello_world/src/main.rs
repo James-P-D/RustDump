81524d80c37447662abf138c2400657ff1157efa
James Dorrington <jdorrington@hotmail.com>
2020-01-06 23:34:17 +0000

    Minor update for getting args for main()

--- src/hello/hello_world/src/main.rs
+++ src/hello/hello_world/src/main.rs
@@ -1,5 +1,14 @@
+use std::env;
+//use std::os;
+
 // CTRL+SHIFT+B to BUILD
 // F5 to RUN
+
 fn main() {
-    println!("Hello, world!!!!");
+    println!("Hello, world   Is it working!!!!");
+
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    ::std::process::exit(1); 
 }
